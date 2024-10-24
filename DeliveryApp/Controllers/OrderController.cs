d6e8e014d39f351e34663784895dfc5f4e2d0bb2
sergiuscain <sergiussmithe@gmail.com>
2024-10-24 17:52:25 +0300

    Создал модель заказа, перенёс Header в отдельный файл.

---
+++ DeliveryApp/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Create(Order)
+    }
+}
